899df4255a9477a8424495dd673f46b943617709
HazemFelhi <hazemfelhi99@gmail.com>
2026-07-04 20:49:40 +0100

    Fix import paths for Spring annotations

--- src/main/java/com/example/microservice/HelloController.java
+++ src/main/java/com/example/microservice/HelloController.java
@@ -1,7 +1,7 @@
 package com.example.microservice;
 
-import org.springframework.web.bind.GetMapping;
-import org.springframework.web.bind.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
